042e13a836a1696b2130198c5d92a62d5a9bfb11
noshinbashar <noshinbashar027@gmail.com>
2024-02-20 23:22:34 -0800

    added component folders

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import "./Header.scss";
+
+function Header() {
+    return (
+        <header className="header">
+            <p>BrainFlix Logo</p>
+
+        </header>
+    );
+}
+
+export default Header;
