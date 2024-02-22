67cc652ec8ec7b4e250dfbde5326cccf62b1f44b
noshinbashar <noshinbashar027@gmail.com>
2024-02-21 23:45:47 -0800

    build header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,37 @@
 import "./Header.scss";
+import Logo from "./../../assets/Images/BrainFlix-logo.svg"
+import SearchImage from "./../../assets/Images/search.svg"
+import ImageIcon from "./../../assets/Images/Mohan-muruge.jpg"
+import UploadImage from "./../../assets/Images/upload.svg"
 
 function Header() {
     return (
+        <>
         <header className="header">
-            <p>BrainFlix Logo</p>
+            
+            <div className="logo">
+            <img className="logo__image" src={Logo} alt="Hero Image" ></img>
+            </div>
+
+            <div className="box">
+                <img className="box__image" src={SearchImage} alt="Search Image" ></img>
+                <input className="box__input" type="text" placeholder="Search" />
+            </div>
+
+            <div className="icon">
+            <img className="icon__image" src={ImageIcon} alt="Icon Image" ></img> 
+            </div>
+
+            <div className="button">
+                <button className="button__text">UPLOAD</button>
+                <img className="button__image" src={UploadImage} alt="Upload Image" ></img>
+            </div>
+
+
+            
 
         </header>
+        </>
     );
 }
 
