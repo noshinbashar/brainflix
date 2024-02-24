4ea7ba80949086071a2202a3c406dde223bf266c
noshinbashar <noshinbashar027@gmail.com>
2024-02-24 12:04:02 -0800

    added the comments

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,20 @@ import Video from "./components/Video/Video";
 import videoData from "../src/Data/video-details.json";
 import { useState } from "react";
 import NewComment from "./components/Newcomment/Newcomment";
+import Comments from "./components/Comments/Comments";
 
 function App() {
 
   const [selectedVideo, setSelectedVideo] = useState(videoData[0]);
-  // const [selectedTitle, setSelectedTitle] = useState(videoData[0]);
+  const [comments, setComments] = useState(videoData[0]);
   
   return (
     <div className="App">
       <Header />
       <Video selectedVideo={selectedVideo} />
       <NewComment />
+      <Comments Comments={comments}/>
+      
     </div>
   );
 }
---
+++ src/components/Comments/Comments.jsx
@@ -0,0 +1,38 @@
+
+function Comments(props) {
+    console.log(props)
+    return(
+        <>
+        <p>Comments</p>
+        <ul>
+            {props.Comments.comments.map((item) => {
+                return(
+                    <>
+                    <li key={item.id}>
+                        <p>{item.name}</p>
+                        <p>{item.comment}</p>
+                        {/* <p>{item.likes}</p> */}
+                        <p>{item.timestamp}</p>
+
+                    </li>
+
+
+
+                    </>
+                )
+            })}
+
+        </ul>
+        </>
+
+    )
+}
+
+export default Comments
+
+// "id": "35bba08b-1b51-4153-ba7e-6da76b5ec1b9",
+// "name": "Noah Duncan",
+// "comment": "Your insights into the future of AI are enlightening! The intersection of technology and ethics is particularly thought-provoking. Keep us updated on the tech front!",
+// "likes": 0,
+// "timestamp": 1691731062000
+
