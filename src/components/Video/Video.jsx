f9f07c86f9084d89a5dfdc7adad688a2a41cde1d
noshinbashar <noshinbashar027@gmail.com>
2024-02-25 14:37:25 -0800

    made the video section responsive, minor adjustments left

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -10,13 +10,15 @@ function Video(props) {
   };
     return(
       <>
+      <div className="videocontainer">
         <video
-      className="video"
+      className="videocontainer__video"
       poster={props.selectedVideo.image}
       controls
     ></video>
+        </div>
 
-      <div className="title">
+      <div className="videoName">
         <h1>{props.selectedVideo.title}</h1>
       </div>
 
@@ -37,15 +39,3 @@ function Video(props) {
 }
 
 export default Video
-
-{/* "id": "84e96018-4022-434e-80bf-000ce4cd12b8",
-      "title": "The Future of Artificial Intelligence",
-      "channel": "Aiden Thompson",
-      "image": "https://unit-3-project-api-0a5620414506.herokuapp.com/images/image0.jpg",
-      "description": "Explore the cutting-edge developments and predictions for Artificial Intelligence in the coming years. From revolutionary breakthroughs in machine learning to the ethical considerations influencing AI advancements, this exploration transcends the boundaries of mere speculation. Join us on a journey that navigates the intricate interplay between innovation, ethics, and the ever-evolving tech frontier.",
-      "views": "980,544",
-      "likes": "22,479",
-      "duration": "4:01",
-      "video": "https://unit-3-project-api-0a5620414506.herokuapp.com/stream",
-      "timestamp": 1691471862000,
-      "comments":  */}
