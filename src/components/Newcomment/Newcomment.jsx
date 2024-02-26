cadfb27fc6f9663d0631edb7d458073c17c2dbf4
noshinbashar <noshinbashar027@gmail.com>
2024-02-25 17:12:12 -0800

    made the newcomment section responsive

--- src/components/Newcomment/Newcomment.jsx
+++ src/components/Newcomment/Newcomment.jsx
@@ -8,13 +8,20 @@ function NewComment() {
         <div className="container">
 
         <p className="text">3 Comments</p>
+
         <div className="commentBox">
+            <div>
             <p className="commentBox__text">JOIN THE CONVERSATION</p>
             <p className="commentBox__input"><input className="commentBox__input--text" type="text" placeholder="Add a new comment"/></p>
+            </div>
+
+            <div>
             <p className="commentBox__btn">
                 <button className="button__text">COMMENT</button>
                 <img className="button__image" src={CommentImage} alt="Comment" ></img>
             </p>
+            </div>
+            
             <img className="commentBox__image" src={Icon} alt="Icon" ></img> 
             
         </div>
