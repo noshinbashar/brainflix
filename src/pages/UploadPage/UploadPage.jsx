279761dc2f4ca7da45f3188b1931f6e02413f86d
noshinbashar <noshinbashar027@gmail.com>
2024-03-19 00:21:06 -0700

    created development branch

--- src/pages/UploadPage/UploadPage.jsx
+++ src/pages/UploadPage/UploadPage.jsx
@@ -1,61 +1,79 @@
 import "./UploadPage.scss"
 import Preview from "../../assets/Images/Upload-video-preview.jpg"
 import Publish from "../../assets/Images/publish.svg"
-import { Link, useNavigate } from "react-router-dom"
+import { Link, Navigate } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
+function UploadPage() {
+    const [published, setPublished] = useState(false);
+    const [redirect, setRedirect] = useState(false);
 
-function UploadPage () {
-    const navigate = useNavigate();
+    useEffect(() => {
+        document.title = "Upload";
+    }, []);
 
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    alert("Thank you for uploading your video.");
-    navigate('/');
-  }
-    
+    const handlePublish = (event) => {
+        event.preventDefault();
+        let newTitle = event.target.title.value;
+        let newDescription = event.target.description.value;
+
+        axios.post("http://localhost:8080/videos", {
+            title: newTitle,
+            description: newDescription
+        })
+            .then(response => {
+                setPublished(true);
+                setTimeout(() => {
+                    setRedirect(true);
+                }, 1000);
+            });
+    }
 
     return (
         <>
-        <div className="preview">
+            <div className="preview">
                 <h1 className="preview-title">Upload Video</h1>
 
-        <div className="upload-video-container">
+                <form className="upload__form" onSubmit={handlePublish}>
+                    <div className="upload-video-container">
 
-            <div className="preview-imagebox"> 
-                <p className="preview-text">VIDEO THUMBNAIL</p>
-                <img className="preview-image" src={Preview} alt="PreviewImage" ></img>
-            </div>
+                        <div className="preview-imagebox">
+                            <p className="preview-text">VIDEO THUMBNAIL</p>
+                            <img className="preview-image" src={Preview} alt="PreviewImage" ></img>
+                        </div>
 
-            <div className="preview-boxs">
-                <p className="preview-text">TITLE YOUR VIDEO</p>
-                <div className="preview-boxone">
-                <input className="preview-input" type="text" placeholder="Add a title to your video"/>
-                </div>
-          
-                <p className="preview-text">ADD A VIDEO DESCRIPTION</p>
-                <div className="preview-boxtwo">
-                <input className="preview-input" type="text" placeholder="Add a description to your video"/>
-                </div>
-            </div>
+                        <div className="preview-boxs">
+                            <p className="preview-text">TITLE YOUR VIDEO</p>
+                            <div className="preview-boxone">
+                                <input className="preview-input" type="text" placeholder="Add a title to your video" name="title" />
+                            </div>
 
-        </div>
-            
-        <div className="button-container">
+                            <p className="preview-text">ADD A VIDEO DESCRIPTION</p>
+                            <div className="preview-boxtwo">
+                                <input className="preview-input" type="text" placeholder="Add a description to your video" name="description" />
+                            </div>
+                        </div>
 
-            <div className="preview-button">
-                <form onSubmit={handleFormSubmit}>
-                    <button className="preview-button__text">PUBLISH</button>
-                    <img className="preview-button__icon" src={Publish} alt="Publish" ></img>
+                    </div>
+
+                    <div className="button-container">
+
+                        <div className="preview-button">
+                            <button type="submit" className="preview-button__text">
+                                {published ? "Success" : "Publish"}
+                            </button>
+                            {redirect && <Navigate to='/' />}
+                            <img className="preview-button__icon" src={Publish} alt="Publish" />
+                        </div>
+
+                        <Link to={"/"} className="link">
+                            <p className="preview-button__cancel">CANCEL</p>
+                        </Link>
+                    </div>
+
                 </form>
             </div>
-
-            <Link to={"/"} className="link">   
-                <p className="preview-button__cancel">CANCEL</p>
-            </Link> 
-        </div>
-
-
-        </div>
         </>
     )
 }
